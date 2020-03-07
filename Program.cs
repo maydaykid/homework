d1bc9df4b86055b10b907352efb3a18c631ddf08
maydaykid <61896904+maydaykid@users.noreply.github.com>
2020-03-07 15:05:08 +1000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace объявление_переменных
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            int day;
+            byte planets;
+            float time;
+            char sinNumber;
+            string poem;
+            bool life;
+            bool death;
+            string name;
+            bool end;
+        }
+    }
+}
